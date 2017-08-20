a71ff5a94ca41d5dc5e5e17d25ac5ec6a6432330
Palash Mondal <palashm@live.in>
2017-08-20 21:28:13 +0530

    Update repeat.js

--- 04 - Additional String Improvements/repeat.js
+++ 04 - Additional String Improvements/repeat.js
@@ -1,3 +1,10 @@
+/**
+    Method  :   String.prototype.repeat()
+    Purpose :   Returns a new string which contains the specified number of copies of the string 
+                on which it was called, concatenated together.
+    Link    :   https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
+*/
+
 // Function to repeat character N times
 function repeatCharacter(char, n = 10) {
     const text = char.repeat( n );
@@ -5,12 +12,12 @@ function repeatCharacter(char, n = 10) {
 }
 
 // Show `🔥` emoji 10 times
-repeatCharacter(🔥);
+repeatCharacter('🔥');
 // Output
 // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
 
 // Show `🔥` emoji 5 times
-repeatCharacter(🔥, 5);
+repeatCharacter('🔥', 5);
 // Output
 // 🔥🔥🔥🔥🔥
 
